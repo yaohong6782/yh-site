6dac1b57bc6c7157db3d5e26973a41cab5bd5fe7
yaohong <yaohong6782@gmail.com>
2021-12-22 03:42:52 +0800

    redesigning in progress

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,16 +1,23 @@
 import React from "react";
-import "./About.css"; 
-import vim from '../../images/vim.png'
+import "./About.css";
+import vim from "../../images/vim.png";
+import bg from "../../images/code-image.png";
 
 const About = () => {
   return (
-    <div className="about-wrapper">
-      <div className="about">
-        <h2>Hi, I'm</h2>
-        <h1>Yao Hong</h1>
-        <h2>an aspiring Software Developer</h2>
+    <div className="main-container">
+      <div className="left-side">
+        <div className="left-wrapper">
+          <h2 classname="left-name">Hi, I am</h2>
+          <h1 className="name">Yao Hong</h1>
+          <div classname="left-desc">An aspiring Software Developer</div>
+        </div>
       </div>
-      <img src={vim} alt="myImage" className="vim-img"></img>
+
+      <div classname="right-side">
+      
+        <img src={vim} alt="myImage" className="vim-img"></img>
+      </div>
     </div>
   );
 };
--- src/components/introduction/Introduction.js
+++ src/components/introduction/Introduction.js
@@ -1,31 +1,12 @@
 import React from "react";
-import "./introduction.css";
+import "./Introduction.css";
 
 const Introduction = () => {
   return (
-    <div className="i-container">
-      <h1 className="i-name">Stuffs </h1>
-      <div className="l-r-container">
-        <div className="left">
-          <div className="left-wrapper">
-            <div className="i-title">
-              <div className="i-title-wrapper">
-                <div className="i-title-item">Programming languages</div>
-
-
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="right"></div>
-        <div className="right-wrapper">
-        <div className="i-title-item">Stuffs</div>
-
-        </div>
+      <div className="a">
+          about
       </div>
-    </div>
-  );
+  )
 };
 
 export default Introduction;
