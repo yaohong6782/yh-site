a88d85213b1e415ebc3468fc28767cc18a060cfc
yaohong <yaohong6782@gmail.com>
2021-12-23 11:55:10 +0800

    projectcard progressed, styling and positioning

--- src/components/body/projects/ProjectCard.js
+++ src/components/body/projects/ProjectCard.js
@@ -8,19 +8,19 @@ const ProjectCard = ({ props }) => {
       <div className="project-info">
         <label className="project-title">{props.title}</label>
         <div className="project-links">
-            {props.github && (
-                <a className="project-link" href={props.github} >
-                    <div className="link-button">
-                    <i class="devicon-github-original colored"></i>GitHub
-                    </div>
-                </a>
-            )}
+          {props.github && (
+            <a className="project-link" href={props.github}>
+              <div className="link-button">
+                <i class="devicon-github-original colored"></i>GitHub
+              </div>
+            </a>
+          )}
         </div>
         <p>{props.about}</p>
         <div className="project-tags">
-            {props.tags.map((tag)=>{
-                return (<label className="tag">{tag}</label>)
-            })}
+          {props.tags.map((tag) => {
+            return <label className="tag">{tag}</label>;
+          })}
         </div>
       </div>
       <img src={props.image} className="project-photo" alt=""></img>
