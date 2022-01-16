3241987f351536577fc7b8538d5ad6a0330242ba
yaohong <yaohong6782@gmail.com>
2022-01-16 19:54:03 +0800

    setting up experiences section

--- src/components/body/experience/Experience.js
+++ src/components/body/experience/Experience.js
@@ -3,7 +3,12 @@ import React from "react";
 const Work = () => {
   return (
     <div>
-      work experience here
+    
+      <h2>Onwards Media Group</h2>
+      <p>Software developer Intern</p>
+      <h2>Changi Airport Group</h2>
+      <p>Student project liased with CAG</p>
+
     </div>
   );
 };
