1af0f54bcec7a8f8a170e4521efde9ae18cc2297
yaohong <yaohong6782@gmail.com>
2022-01-22 18:27:32 +0800

    changed experiences to variabe location for ease of updating

--- src/components/body/experience/Experience.js
+++ src/components/body/experience/Experience.js
@@ -1,14 +1,28 @@
 import React from "react";
 
 const Work = () => {
-  return (
+
+    const ExpList = [
+        {
+            id:1,
+            exp:"Onwards Media Group Pte Ltd",
+            department:"Freeboh",
+            role:"Intern Software Developer"
+        },
+        {
+            id:2,
+            exp:"Student Project liased with Changi Airport Group",
+            department:"",
+            role:"Software Developer"
+        }
+    ]
+    return (
     <div>
-    
+      {/* css styling to be done  */}
       <h2>Onwards Media Group</h2>
       <p>Software developer Intern</p>
       <h2>Changi Airport Group</h2>
       <p>Student project liased with CAG</p>
-
     </div>
   );
 };
